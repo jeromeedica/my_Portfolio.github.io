22563b0aeb20b5e8d931ca255f51363c77695012
x <80389604+jeromeedica@users.noreply.github.com>
2021-07-28 18:56:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+function fadeOut() {
+    const story = document.getElementById('story');
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+    story.style.opacity = '2';
 }
